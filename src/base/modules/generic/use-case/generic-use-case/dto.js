46d14a8271bfecb3e58f5360cc5c3b9c20e74827
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-10-11 21:42:11 -0300

    fix: remove js files in base use-case

--- src/base/modules/generic/use-case/generic-use-case/dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GenericResponseDTO = exports.GenericDTO = void 0;
-class GenericDTO {
-}
-exports.GenericDTO = GenericDTO;
-class GenericResponseDTO {
-}
-exports.GenericResponseDTO = GenericResponseDTO;
