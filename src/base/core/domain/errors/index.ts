3ce920b1d5c65a2e022555d0cfbe8e8a6903ae38
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-01-16 21:47:09 -0300

    removing errors files

--- src/base/core/domain/errors/index.ts
+++ src/base/core/domain/errors/index.ts
@@ -1,19 +1,5 @@
 import { DomainError } from './DomainError';
 import { InternalDomainError } from './InternalDomainError';
-import { InvalidFieldSizeError } from './InvalidFieldSizeError';
-import { InvalidFieldTypeError } from './InvalidFieldTypeError';
-import { MissingRequiredFieldError } from './MissingRequiredFieldError';
-import { MissingRequiredInternalFieldError } from './MissingRequiredInternalFieldError';
-import { StringShouldContainOnlyDigitsError } from './StringShouldContainOnlyDigitsError';
 import { UseCaseError } from './UseCaseError';
 
-export {
-  DomainError,
-  InternalDomainError,
-  InvalidFieldSizeError,
-  InvalidFieldTypeError,
-  MissingRequiredFieldError,
-  MissingRequiredInternalFieldError,
-  StringShouldContainOnlyDigitsError,
-  UseCaseError,
-};
+export { DomainError, InternalDomainError, UseCaseError };
